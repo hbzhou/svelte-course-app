9e67dfe42747b85f8352f97748344227360c26ea
Jeremy Zhou <jeremygilbert@126.com>
2023-08-07 22:49:36 +0800

    adding modal for Edit author

---
+++ src/lib/common/Modal.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	export let showModal: boolean; // boolean
+
+	let dialog: HTMLDialogElement;
+
+	$: if (dialog && showModal) dialog.showModal();
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog
+	bind:this={dialog}
+	on:close={() => (showModal = false)}
+	on:click|self={() => dialog.close()}
+>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div on:click|stopPropagation>
+		<slot name="header" />
+		<hr />
+		<slot />
+		<hr />
+		<div class="flex justify-end mr-8 items-center">
+			<button
+				on:click={() => dialog.close()}
+				class="border-2 border-solid rounded-md w-24 h-8 ml-2 mt-2 bg-teal-500">Cancel</button
+			>
+			<button
+				on:click={() => dialog.close()}
+				class="border-2 border-solid rounded-md w-24 h-8 ml-2 mt-2 bg-emerald-600">Save</button
+			>
+		</div>
+	</div>
+</dialog>
+
+<style>
+	dialog {
+		max-width: 48em;
+		min-width: 32em;
+		max-height: 48em;
+		min-height: 12em;
+		border-radius: 0.2em;
+		border: none;
+		padding: 0;
+	}
+	dialog::backdrop {
+		background: rgba(0, 0, 0, 0.3);
+	}
+	dialog > div {
+		padding: 1em;
+	}
+	dialog[open] {
+		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+	}
+	@keyframes zoom {
+		from {
+			transform: scale(0.95);
+		}
+		to {
+			transform: scale(1);
+		}
+	}
+	dialog[open]::backdrop {
+		animation: fade 0.2s ease-out;
+	}
+	@keyframes fade {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+	button {
+		display: inline-block;
+	}
+</style>
--- src/routes/authors/+page.svelte
+++ src/routes/authors/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import Modal from '$lib/common/Modal.svelte';
 	import { authors } from '../../store/store';
+	let showModal = false;
 </script>
 
 <div class="border-solid border-2 border-pink-400 m-4">
@@ -13,8 +15,9 @@
 		{#each $authors as author (author.id)}
 			<div class="text-2xl">{author.name}</div>
 			<div>
-				<button class="border-solid border-2 p-1 border-purple-400 w-28 mx-2 rounded-md"
-					>Edit</button
+				<button
+					class="border-solid border-2 p-1 border-purple-400 w-28 mx-2 rounded-md"
+					on:click={() => (showModal = true)}>Edit</button
 				>
 				<button class="border-2 border-solid p-1 border-purple-400 w-28 mx-2 rounded-md"
 					>Remove</button
@@ -22,4 +25,14 @@
 			</div>
 		{/each}
 	</div>
+	<Modal bind:showModal>
+		<div slot="header" class="flex justify-between items-center">
+			<h2 class=" text-2xl font-bold">Edit Author</h2>
+			<button on:click={() => (showModal = false)}>x</button>
+		</div>
+		<div class="flex items-center my-8">
+			<div class="mx-2 font-bold">AuthorName:</div>
+			<input type="text" name="name" class="border-2 border-solid w-72 h-8 rounded-md" />
+		</div>
+	</Modal>
 </div>
